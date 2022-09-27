f5355f07ef467b78ec9ea6e8a57a37c3860c4413
MOJEED-OLORUNTOSIN <mojeedoloruntosin@gmail.com>
2022-09-26 21:50:28 -0700

    struct 8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
-
 def multiple_returns(sentence):
-    """Returns tuple with len of string and first char"""
-    length = len(sentence)
-    if sentence:
-        first = sentence[0]
-    else:
-        first = None
-        return (length, first)
+    """Returns the length of a string and its first character."""
+    if sentence == "":
+        return (0, None)
+    return (len(sentence), sentence[0])
